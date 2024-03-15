73880993230364703185a07b85eaa820dae3efcb
zobayerdev <zobayer.dev@gmail.com>
2024-03-15 23:35:09 +0600

    code complete

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# ostad_live_test
-It's a ostad live exam code.
+A sample command-line application with an entrypoint in `bin/`, library code
+in `lib/`, and example unit test in `test/`.
---
+++ bin/main.dart
@@ -0,0 +1,33 @@
+// Function to display details of each fruit
+void displayFruitDetails(List<Map<String, dynamic>> fruits) {
+  for (var fruit in fruits) {
+    print(
+        "Name: ${fruit['name']}, Color: ${fruit['color']}, Price: \$${fruit['price']}");
+  }
+}
+
+// Function to apply discount to each fruit price
+void applyPriceDiscount(List<Map<String, dynamic>> fruits, double discount) {
+  for (var fruit in fruits) {
+    fruit['price'] = (fruit['price'] as double) * (1 - discount);
+  }
+}
+
+void main() {
+  List<Map<String, dynamic>> fruits = [
+    {"name": "Apple", "color": "Red", "price": 2.5},
+    {"name": "Banana", "color": "Yellow", "price": 1.0},
+    {"name": "Grapes", "color": "Purple", "price": 3.0},
+  ];
+
+  // show the orginal list of fruits.
+  print("\nOriginal Fruit Details before Discount:");
+  displayFruitDetails(fruits);
+
+  double discount = 0.1; // apply 10% discount question wise
+  applyPriceDiscount(fruits, discount);
+
+  // applying discount and show the result
+  print("\nFruit Details After Applying 10% Discount:");
+  displayFruitDetails(fruits);
+}
